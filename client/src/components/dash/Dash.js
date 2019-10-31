bcc21adc207500360c383310c2f14d4291da5853
Henry Doan <henrydoan96@gmail.com>
2019-10-31 17:47:54 -0600

    button logic

--- client/src/components/dash/Dash.js
+++ client/src/components/dash/Dash.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
+import { Button, Icon } from 'semantic-ui-react'
+
 
 
 const Dash = () => {
   const [doorOpen, setDoorOpen] = useState(false);
 
+  const isOpen = () => {
+    if (doorOpen) {
+      return <Button onClick={() => setDoorOpen(!doorOpen)}>
+        <Button.Content>Close</Button.Content>
+      </Button>
+    } else {
+      return <Button onClick={() => setDoorOpen(!doorOpen)}>
+        <Button.Content>Open</Button.Content>
+      </Button>
+    }
+  }
+
   return(
     <>
       <h1>Dashboard</h1>
 
-      <button>Open</button>
+      {isOpen()}
     </>
 
   )
